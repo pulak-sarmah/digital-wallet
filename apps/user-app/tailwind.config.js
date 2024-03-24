4901eaae0261a69b4cd7b87bb5bd587d85d8277a
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-24 21:06:32 +0530

    Add UI source files to Tailwind config

--- apps/user-app/tailwind.config.js
+++ apps/user-app/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
