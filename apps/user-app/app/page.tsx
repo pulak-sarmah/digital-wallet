e77fc435f0c5b2f2546503c55945acbf7e9322bd
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-29 19:22:16 +0530

    setup auth, store

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
 import React from "react";
+import { Appbar } from "@repo/ui/appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const page = () => {
-  return <div className=" font-thin text-4xl">page</div>;
+  const session = useSession();
+  return (
+    <div className="text-4xl font-thin ">
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+    </div>
+  );
 };
 
 export default page;
